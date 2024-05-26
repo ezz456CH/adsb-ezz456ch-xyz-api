8ecec1f5a619ba17337cf86c1d188df98fbbb59c
ezz456CH <ezz456chgmok@gmail.com>
2024-05-26 19:39:20 +0700

    set a maximum radius of 250 nmi in circle endpoints

--- controllers/circlecontroller.js
+++ controllers/circlecontroller.js
@@ -3,13 +3,19 @@ const axios = require('axios');
 const re_api_url = process.env.re_api_url;
 
 exports.getCircle = async (req, res) => {
+    const radius = parseFloat(req.params.radius_nmi);
+
+    if (radius > 250) {
+        return res.status(400).json({ error: 'Radius must not be more than 250 nautical miles' });
+    }
+
     try {
-        const response = await axios.get(`${re_api_url}/?circle=${req.params.lat},${req.params.lon},${req.params.radius_nmi}&jv2`);
+        const response = await axios.get(`${re_api_url}/?circle=${req.params.lat},${req.params.lon},${radius}&jv2`);
         const data = response.data;
         res.json(data);
     } catch (error) {
         console.log(error);
-        
+
         if (error.code === 'ETIMEDOUT' || error.response?.status === 504) {
             res.status(504).json({ error: 'Gateway Timeout' });
         } else {
